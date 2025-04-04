2b42d8008190f1c3e22c61aec8799dd9344a6a52
SandeepKumar <sandeepkusolanki@gamil.com>
2025-04-05 01:03:43 +0530

    setVis

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,23 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
+  console.log(props)
   const [vis , setVis] = useState(false)
+
+  function handleClick(){
+    setVis(true);
+  }
   return (
+
     <div className="App" id="main">
       {/* // Do not alter the main div */}
       {vis === false ? (
         <button 
-          onClick={setVis(true)}
+        onClick={handleClick}
         id="click">Show</button>
+        
       ):(
-        <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+          <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
         )
       }
     </div>
