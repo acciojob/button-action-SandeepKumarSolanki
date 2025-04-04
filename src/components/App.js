10c5a0cb01bbdd544a441ddbed22ed71ac84581d
SandeepKumar <sandeepkusolanki@gamil.com>
2025-04-05 00:56:02 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
-
+  const [vis , setVis] = useState(false)
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      {vis === false ? (
+        <button 
+          onClick={setVis(true)}
+        id="click">Show</button>
+      ):(
+        <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+        )
+      }
     </div>
   );
 }
