6743d49636c5452a7f08f2cabe607b9ea919f8a0
SandeepKumar <sandeepkusolanki@gamil.com>
2025-06-13 18:34:57 +0530

    sixth commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,25 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
-  console.log(props)
-  const [vis, setVis] = useState(false)
+  const [vis, setVis] = useState(true);
 
   function handleClick() {
     setVis(vis => !vis);
   }
+
   return (
-
     <div className="App" id="main">
-
-      {/* // Do not alter the main div */}
-
-      {vis && <p id="para" className={vis ? "show" : "hide"}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-      }
-
-      <button
-        onClick={handleClick}
-        id="click">{vis ? "Hide" : "Show"}</button>
-
+      {/* Do not alter the main div */}
+      {vis && (
+        <p id="para" className={vis ? "show" : "hide"}>
+          Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+        </p>
+      )}
+      <button onClick={handleClick} id="click">
+        {vis ? "Hide" : "Show"}
+      </button>
     </div>
   );
 }
 
-export default App
+export default App;
